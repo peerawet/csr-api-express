0f5e19566891f9d05696e5d085e04422ecf056e9
peerawet <peerawet1996@gmail.com>
2024-07-14 12:02:50 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { employeesRouter } from "./routers/employeesRouter.js";
+
+
+function init() {
+  dotenv.config();
+
+  const app = express();
+  const port = 4000;
+
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.get("/", (req, res) => {
+    res.send("Hello supabase");
+  });
+
+  app.use("/employees", employeesRouter);
+
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+      limit: "35mb",
+      parameterLimit: 50000,
+    })
+  ),
+    app.use((err, req, res, next) => {
+      if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+        res.status(400).json({ error: "Invalid JSON payload" });
+      } else {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+init();
---
+++ routers/employeesRouter.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import supabase from "../utills/supabase.js";
+
+export const employeesRouter = Router();
+
+employeesRouter.get("/get-employee-kpi", async (req, res) => {
+  try {
+    // Extract department-id query parameter
+    const departmentIds =
+      req.query["department-id"]?.toString().split(",") || [];
+
+    // Construct the query
+    let { data, error } = await supabase
+      .from("employees")
+      .select("*")
+      .in("department_id", departmentIds)
+
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    // Send JSON response
+    res.status(200).json(data);
+  } catch (error) {
+    // Handle any exceptions
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
