47f6a785de80f3c5f1dc83015b813e1e2eec8cdd
peerawet <144383742+peerawet@users.noreply.github.com>
2024-07-14 22:07:36 +0700

    Update supabase.js

--- utills/supabase.js
+++ utills/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-
-const supabaseUrl = 'https://zktsxumtyerxtskmazsk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprdHN4dW10eWVyeHRza21henNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4ODc0MDYsImV4cCI6MjAzNjQ2MzQwNn0.lB9X4Z_a2W4fkOvbTVD81q-AP4Tlde_IrC88CKqAIBw';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
