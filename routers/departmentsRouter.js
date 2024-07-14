eda91ea2ba66c58a91236d439d7c4c3d9030d7d6
peerawet <144383742+peerawet@users.noreply.github.com>
2024-07-14 13:57:39 +0700

    Update departmentsRouter.js

--- routers/departmentsRouter.js
+++ routers/departmentsRouter.js
@@ -1,29 +1,19 @@
 import { Router } from "express";
 import supabase from "../utills/supabase.js";
 
-export const employeesRouter = Router();
+export const departmentsRouter = Router();
 
-employeesRouter.get("/get-employee-kpi", async (req, res) => {
+departmentsRouter.get("/get-department", async (req, res) => {
   try {
-    // Extract department-id query parameter
-    const departmentIds =
-      req.query["department-id"]?.toString().split(",") || [];
-
-    // Construct the query
-    let { data, error } = await supabase
-      .from("employees")
-      .select("*, departments(name, departmental_kpi)")
-      .in("department_id", departmentIds);
+    const { data, error } = await supabase.from("departments").select("*");
 
     if (error) {
-      throw new Error(error.message);
+      throw error;
     }
 
-    // Send JSON response
     res.status(200).json(data);
   } catch (error) {
-    // Handle any exceptions
-    console.error("Error:", error.message);
+    console.error("Error fetching departments:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
